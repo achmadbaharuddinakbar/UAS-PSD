c63f4e8afbf27f5ecc19f312a54b6e3963efd02b
Achmad Baharuddin Akbar <112038717+achmadbaharuddinakbar@users.noreply.github.com>
2023-12-10 23:19:32 +0700

    uas.py

--- uas.py
+++ uas.py
@@ -3,22 +3,30 @@ import pandas as pd
 from streamlit_option_menu import option_menu
 
 # Navigation sidebar
-selected2 = option_menu(None, ["Dataset", "Processing data", "Modelling"], 
+selected2 = option_menu(None, ["Dataset", "Processing data", "Modelling", "Model Validation"], 
     icons=['house', 'cloud-upload', 'list-task', 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal")
 
 # Page: Dataset
 if selected2 == 'Dataset':
     st.title('Deskripsi data')
-    # Your dataset-related code here
+    
+    data1 = pd.read_csv('milknew.csv')
+    st.write(data1)
 
 # Page: Processing data
 if selected2 == 'Processing data':
     st.title('Processing Data')
     st.write("Kami Melakukan Pre-processing data dengan metode Min - Max Scalar")
     st.write("Dengan Hasil Processing Data")
-    data = pd.read_csv('milk_quality_non-imbalance.csv')
-    st.write(data)
+    data2 = pd.read_csv('milk_quality_non-imbalance.csv')
+    st.write(data2)
+    
+    st.title('Processing Data')
+    st.write("Kami Melakukan Pre-processing data dengan metode Min - Max Scalar")
+    st.write("Dengan Hasil Processing Data")
+    data3 = pd.read_csv('milk_quality_imbalance.csv')
+    st.write(data3)
 
 # Page: Modelling
 if selected2 == 'Modelling':
