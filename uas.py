4683bdbd17d7ae4e999e06bf1b476c6610f16030
Achmad Baharuddin Akbar <112038717+achmadbaharuddinakbar@users.noreply.github.com>
2023-12-10 23:27:05 +0700

    uas.py

--- uas.py
+++ uas.py
@@ -10,7 +10,21 @@ selected2 = option_menu(None, ["Dataset", "Processing data", "Modelling", "Model
 # Page: Dataset
 if selected2 == 'Dataset':
     st.title('Deskripsi data')
-    st.write('Deskripsi data')
+    st.write('Untuk mengetahui Kualitas pada Susu')
+    st.write('Data yang saya gunakan disini yaitu tentang Prediksi Kualitas Susu yang saya dapatkan dari kaggle : https://www.kaggle.com/datasets/cpluzshrijayan/milkquality')
+    st.write('Data Kualitas Susu ini merupakan Type Data Numerical.')
+    st.write('Tentang Dataset : ')
+    st.write('Dataset ini dikumpulkan secara manual dari pengamatan. Hal ini dapat membantu dalam membangun model pembelajaran mesin untuk memprediksi kualitas susu.')
+    st.write('Dataset ini terdiri dari 8 variabel independen yaitu :')
+    st.write('1. pH : variabel ini mendefinisikan pH halus susu yang berkisar antara 3 hingga 9.5 maks : 6.25 hingga 6.90')
+    st.write('2. Temperature : variabel ini mendefinisikan Suhu susu yang berkisar dari 34 derajat Celcius hingga 90 derajat Celcius maks : 34 derajat Celcius hingga 45.20 derajat Celcius')
+    st.write('3. Taste : variabel ini mendefinisikan Rasa susu yang merupakan data kategori 0 (Buruk) atau 1 (Baik) maks : 1 (Baik)')
+    st.write('4. Odor : variabel ini mendefinisikan Bau susu yang merupakan data kategori 0 (Buruk) atau 1 (Baik) maks : 0 (Buruk)')
+    st.write('5. Fat : variabel ini mendefinisikan Lemak susu yang merupakan data kategori 0 (Rendah) atau 1 (Tinggi) maks : 1 (Tinggi)')
+    st.write('6. Turbidity : variabel ini mendefinisikan Kekeruhan susu yang merupakan data kategorikal 0 (Rendah) atau 1 (Tinggi) maks : 1 (Tinggi)')
+    st.write('7. Color : variabel ini menentukan Warna susu yang berkisar dari 240 hingga 255 maks : 255')
+    st.write('8. Grade : variabel ini mendefinisikan Grade (Target) susu yang merupakan data kategori Dimana Low (Buruk) atau Medium (Sedang) atau High (Baik)')
+
     data1 = pd.read_csv('milknew.csv')
     st.write(data1)
 
@@ -41,6 +55,11 @@ if selected2 == 'Modelling':
         st.title(' Nilai Akurasi 77,5 %')
     elif pilih == 'ANN':
         st.title(' Nilai Akurasi 62,5%')
+        
+if selected2 == 'Model Validation':
+    st.title('Model Validation')
+
+
 
 # Page: Implementasi
 # Uncomment and complete the code if needed
