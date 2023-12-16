904142cf1a3d2de8cf23f320bd3d889d58540fd7
Achmad Baharuddin Akbar <112038717+achmadbaharuddinakbar@users.noreply.github.com>
2023-12-17 00:07:29 +0700

    uas.py

--- uas.py
+++ uas.py
@@ -121,55 +121,62 @@ if (selected2 == 'Implementasi') :
     st.write('Untuk mengetahui Kualitas pada Susu')
    
 
-    col1,col2,col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)
+
     with col1:
-        ph = st.number_input('Silahkan Masukkan pH  :')
-        temprature = st.number_input('Silahkan Masukkan Suhu  :',0)
-        list_taste = ['Silahkan Pilih Rasa ','Baik','Buruk']
-        taste = st.selectbox('Silahkan Pilih Rasa ', list_taste)
+        ph = st.number_input('Silahkan Masukkan pH:')
+        temprature = st.number_input('Silahkan Masukkan Suhu:', 0)
+
     with col2:
-        list_odor = ['Silahkan Pilih Bau ','Baik','Buruk']
+        list_odor = ['Silahkan Pilih Bau', 'Baik', 'Buruk']
         odor = st.selectbox('Silahkan Pilih Bau susu', list_odor)
-        list_fat = ['Silahkan Pilih Lemak ','Rendah','Tinggi']
-        fat = st.selectbox('Silahkan Pilih Lemak ', list_fat)
+        list_fat = ['Silahkan Pilih Lemak', 'Rendah', 'Tinggi']
+        fat = st.selectbox('Silahkan Pilih Lemak', list_fat)
+
     with col3:
-        list_turbidity = ['Silahkan Pilih Kekeruhan ','Rendah','Tinggi']
-        turbidity = st.selectbox('Silahkan Pilih Kekeruhan ', list_turbidity)
-        colour = st.number_input('Silahkan Masukkan Warna  :',0)
+        list_turbidity = ['Silahkan Pilih Kekeruhan', 'Rendah', 'Tinggi']
+        turbidity = st.selectbox('Silahkan Pilih Kekeruhan', list_turbidity)
+        colour = st.number_input('Silahkan Masukkan Warna:', 0)
 
-    button = st.button('Cek Kualitas Susu', use_container_width = 500, type = 'primary')
-
+    button = st.button('Cek Kualitas Susu', use_container_width=500, type='primary')
+    
     if button:
-        if taste != 'Silahkan Pilih' and odor != 'Silahkan Pilih' and fat != 'Silahkan Pilih' and turbidity != 'Silahkan Pilih' and ph != 0 and temprature != 0 and colour != 0:
-            if taste=='Baik':
-                taste=1
-            if taste=='Buruk':
-                taste=0
-            if odor=='Baik':
-                odor=1
-            if odor=='Buruk':
-                odor=0
-            if fat=='Rendah':
-                fat=0
-            if fat=='Tinggi':
-                fat=1
-            if turbidity=='Rendah':
-                turbidity=0
-            if turbidity=='Tinggi':
-                turbidity=1
-            
-            ph=((ph-3)/(9.5-3))*(1-0)+0
-            temprature=((temprature-34)/(90-34))*(1-0)+0
-            colour=((colour-240)/(255-240))*(1-0)+0
-            #st.write(ph,temprature,taste,odor,fat,turbidity,colour)
+        if odor != 'Silahkan Pilih' and fat != 'Silahkan Pilih' and turbidity != 'Silahkan Pilih' and ph != 0 and temprature != 0 and colour != 0:
+            # Mengubah kategori menjadi angka biner
+            if odor == 'Baik':
+                odor = 1
+            elif odor == 'Buruk':
+                odor = 0
+    
+            if fat == 'Rendah':
+                fat = 0
+            elif fat == 'Tinggi':
+                fat = 1
+    
+            if turbidity == 'Rendah':
+                turbidity = 0
+            elif turbidity == 'Tinggi':
+                turbidity = 1
+    
+            # Normalisasi fitur-fitur
+            ph = ((ph - 3) / (9.5 - 3)) * (1 - 0) + 0
+            temprature = ((temprature - 34) / (90 - 34)) * (1 - 0) + 0
+            colour = ((colour - 240) / (255 - 240)) * (1 - 0) + 0
+    
+            # Melakukan prediksi dengan model Decision Tree yang telah disimpan
             import pickle
-            with open('milkquality.pkl','rb') as read:
-                knn=pickle.load(read)
-            cek=knn.predict([[ph,temprature,taste,odor,fat,turbidity,colour]])
+            with open('decision_tree_model.pkl', 'rb') as read:
+                decision_tree_model = pickle.load(read)
+    
+            # Remove 'taste' column from the input features
+            cek = decision_tree_model.predict([[ph, temprature, odor, fat, turbidity, colour]])
+    
+            # Menampilkan hasil prediksi
             for prediksi in cek:
-                st.write('Kualitas Susu Anda ',prediksi)
+                st.write('Kualitas Susu Anda', prediksi)
         else:
             st.write('ISI KOLOM TERLEBIH DAHULU')
+
             
 if (selected2 == 'About Us') :
     st.title('Kelompok 7')
